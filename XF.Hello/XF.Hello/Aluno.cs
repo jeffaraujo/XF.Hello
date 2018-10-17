23f9a6059238a1bcfe5297e639fdd928a2e0213f
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-17 20:04:33 -0300

    Add project files.

---
+++ XF.Hello/XF.Hello/Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XF.Hello
+{
+    public class Aluno
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public string RM
+        {
+            get
+            {
+                return Id.ToString();
+            }
+        }
+    }
+}
---
+++ XF.Hello/XF.Hello/App.xaml.cs
@@ -0,0 +1,35 @@
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace XF.Hello
+{
+	public partial class App : Application
+	{
+        public static Configuracao ConfigModel { get; set; }
+        public App ()
+		{
+            if (ConfigModel == null) ConfigModel = new Configuracao();
+
+			InitializeComponent();
+
+			MainPage = new NavigationPage(new MainPage());
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
